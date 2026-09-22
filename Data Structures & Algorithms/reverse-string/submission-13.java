e8c5e98283dcb8be17f99a3360063a53e705a6b0
nikolasR1 <nikosoda19@gmail.com>
2026-09-22 09:44:04 -0500

    Add: reverse-string - submission-13

---
+++ Data Structures & Algorithms/reverse-string/submission-13.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Stack <Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length; i++){
+            st.push(s[i]);
+        }
+        int i = 0;
+        while(!st.isEmpty()){
+            s[i] = st.pop();
+            i++;
+        }
+    }
+}
